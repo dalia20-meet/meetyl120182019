5497e7326dc4d132f1f3ebe7ad592a5e29be6771
dalia20-meet <dalia20@meet.mit.edu>
2018-10-17 12:23:16 -0400

    my first commit

---
+++ lab2.py
@@ -0,0 +1,15 @@
+def fun(n):
+	a=n[0]
+	b=n[-1]
+	list1=[a,b]
+	return list1
+
+	list2=[2,3,4,5]
+	fun(list2)
+	print(n)
+
+def fun2(d):
+	 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+ for i in d: 
+		if i < 5:
+			print(i)
